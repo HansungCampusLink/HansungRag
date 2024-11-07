6ad393466eaa24b900f8e714f04cc1a44b47cf2c
luizy <luizy991212@gmail.com>
2024-11-08 03:52:47 +0900

    feat: Pinecone 2.1.0 VectorStore add
    Pinecone 2.1.0 VectorStore based on code below
    - code : https://github.com/spring-projects/spring-ai/blob/8703e994893f7e20f2df1fc5d204462c1e6d2769/vector-stores/spring-ai-pinecone-store/src/main/java/org/springframework/ai/vectorstore/PineconeVectorStore.java
    - related PR : https://github.com/spring-projects/spring-ai/pull/1501

--- src/main/java/com/example/demo/HansungRagApplication.java
+++ src/main/java/com/example/demo/HansungRagApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.vector.PineconeVectorStore;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +27,25 @@ public class HansungRagApplication {
 	@Bean
 	QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore) {
 		return new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults());
+	}
+
+	@Bean
+	VectorStore vectorStore(PineconeVectorStore.PineconeVectorStoreConfig pineconeVectorStoreConfig, EmbeddingModel embeddingModel) {
+		return new PineconeVectorStore(pineconeVectorStoreConfig, embeddingModel);
+	}
+
+	@Bean
+	PineconeVectorStore.PineconeVectorStoreConfig pineconeVectorStoreConfig(
+			@Value("${spring.ai.vectorstore.pinecone.api-key}") String apiKey,
+			@Value("${spring.ai.vectorstore.pinecone.index-name}") String indexName,
+			@Value("${spring.ai.vectorstore.pinecone.namespace}") String namespace
+	) {
+		PineconeVectorStore.PineconeVectorStoreConfig.Builder builder = PineconeVectorStore.PineconeVectorStoreConfig.builder()
+				.withApiKey(apiKey)
+				.withIndexName(indexName)
+				.withNamespace(namespace);
+
+		return new PineconeVectorStore.PineconeVectorStoreConfig(builder);
 	}
 
 }
